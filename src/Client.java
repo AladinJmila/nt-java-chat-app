1cbe6d3329098497f98f56e36aaaff7800beb7f9
Alaeddine Jmila <jmila.aladin@gmail.com>
2025-01-02 11:21:23 +0000

    Refactor Client

--- src/Client.java
+++ src/Client.java
@@ -5,33 +5,44 @@ import java.io.PrintWriter;
 import java.net.Socket;
 
 public class Client {
-    private static String host = "localhost";
-    private static int port = 8888;
+    private String host = "localhost";
+    private int port = 8888;
 
-    public static void main(String[] args) {
-        try (Socket socket = new Socket(host, port)) {
-            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
-            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+    public void init() {
+        try (Socket socket = new Socket(host, port);
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
+                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);) {
 
-            new Thread(() -> {
-                try {
-                    String serverMessage;
-                    while ((serverMessage = in.readLine()) != null) {
-                        System.out.println("Server message: " + serverMessage);
-                    }
-                } catch (IOException e) {
-                    System.err.println(e);
-                }
-            }).start();
-
-            String clientMessage;
-            while ((clientMessage = consoleReader.readLine()) != null) {
-                out.println(clientMessage);
-            }
+            listenToIncomingMessages(in);
+            handleClientInput(consoleReader, out);
 
         } catch (IOException e) {
             System.err.println(e);
         }
+    }
+
+    private void listenToIncomingMessages(BufferedReader in) {
+        new Thread(() -> {
+            try {
+                String serverMessage;
+                while ((serverMessage = in.readLine()) != null) {
+                    System.out.println(serverMessage);
+                }
+            } catch (IOException e) {
+                System.err.println(e);
+            }
+        }).start();
+    }
+
+    private void handleClientInput(BufferedReader in, PrintWriter out) throws IOException {
+        String clientMessage;
+        while ((clientMessage = in.readLine()) != null) {
+            out.println(clientMessage);
+        }
+    }
+
+    public static void main(String[] args) {
+        new Client().init();
     }
 }
