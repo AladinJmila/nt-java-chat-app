5a83845537756e25a89dcbe019760d4c4ae28b6e
Alaeddine Jmila <jmila.aladin@gmail.com>
2025-01-02 10:58:56 +0000

    Allow client to leave the chat

--- src/Server.java
+++ src/Server.java
@@ -9,13 +9,16 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Server {
-    public static final int PORT = 8888;
+    public final int PORT = 8888;
+    private boolean running = true;
+    private ServerSocket serverSocket;
 
-    public static void main(String[] args) throws Exception {
+    private void init() {
         try (ExecutorService pool = Executors.newVirtualThreadPerTaskExecutor()) {
             try (ServerSocket server = new ServerSocket(PORT)) {
+                serverSocket = server;
                 System.out.println("Listening for connections on port " + PORT + "...");
-                while (true) {
+                while (running) {
                     Socket clientSocket = server.accept();
                     Runnable task = new ClientHandler(clientSocket);
                     pool.execute(task);
@@ -24,14 +27,17 @@ public class Server {
                 System.out.println("Can't connect to host");
                 e.printStackTrace();
             } catch (IOException e) {
-                System.err.println(e);
+                e.printStackTrace();
+                if (!serverSocket.isClosed())
+                    serverSocket.close();
+                running = false;
             }
         } catch (Exception e) {
-            System.err.println(e);
+            e.printStackTrace();
         }
     }
 
-    private static class ClientHandler implements Runnable {
+    private class ClientHandler implements Runnable {
         SoundNotification notification = new SoundNotification();
         private Socket clientSocket;
 
@@ -41,20 +47,30 @@ public class Server {
 
         @Override
         public void run() {
-            try {
-                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            try (BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                    PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);) {
 
                 out.println("Hello from server");
 
                 String clientMessage;
-                while ((clientMessage = in.readLine()) != null) {
+                while ((!clientSocket.isClosed()) && ((clientMessage = in.readLine()) != null)) {
+                    if (clientMessage.startsWith("/q")) {
+                        out.println("You're being diconnected...");
+                        clientSocket.close();
+                        break;
+                    }
                     System.out.println(clientMessage);
                     notification.play();
                 }
             } catch (IOException e) {
-                System.err.println(e);
+                e.printStackTrace();
             }
         }
+
     }
+
+    public static void main(String[] args) throws Exception {
+        new Server().init();
+    }
+
 }
